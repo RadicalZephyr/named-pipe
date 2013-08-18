995ac187bd2ddf80a9ab605abeba6f84159ee488
Geoff Shannon <earthlingzephyr@gmail.com>
2013-08-17 18:24:38 -0700

    Add an echo server program

---
+++ echoserver.cpp
@@ -0,0 +1,36 @@
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include "testdata.h"
+#include "named_pipe.hpp"
+
+#define BUFSIZE 512
+
+int main() {
+  printf("Starting the echo server...");
+  boost::interprocess::named_pipe_server server(pipename);
+  boost::interprocess::named_pipe pipe = server.accept();
+
+  printf("\nReceived pipe connection!");
+
+  const char *exit = "exit";
+  const std::string yousaid("You said: ");
+
+  char buffer[BUFSIZE];
+  int read = 0;
+
+  printf("\nStarting to echo...\n--\n");
+  bool running = true;
+  while (running) {
+    read = pipe.read(buffer, BUFSIZE);
+    printf("received: '%s'\n", buffer);
+    pipe.write((yousaid+buffer).c_str(), yousaid.length()+read);
+    if (strncmp(exit, buffer, 4)) {
+        running = false;
+        pipe.write("Goodbye!!", 9);
+      }
+  }
+  printf("\nDone echoing.\n");
+}
